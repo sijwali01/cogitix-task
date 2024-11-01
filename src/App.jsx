dadab790529f3a165e2caecedf3adc49ff907234
sijwali01 <manishsijwalil@gmail.com>
2024-11-01 11:40:52 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Heading from './Header/Heading'
+import Body from './Body/Body'
+
+const App = () => {
+  return (
+    <>
+    <Heading/>
+    <Body/>
+    
+    </>
+  )
+}
+
+export default App
